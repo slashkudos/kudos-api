05d19ef0913e3b61c68702453deaf33c859858b1
Philip Gai <PhilipMGai@gmail.com>
2021-11-19 08:01:10 +0000

    Create aws profile on create

--- scripts/Update-DevContainer.ps1
+++ scripts/Update-DevContainer.ps1
@@ -1,4 +1,12 @@
+#!/bin/pwsh
+
 # Post devcontainer create script to run
 
 git config --global pull.rebase true
 sudo npm install -g @aws-amplify/cli
+
+echo @"
+[default]
+aws_access_key_id = $env:AWS_ACCESS_KEY_ID
+aws_secret_access_key = $env:AWS_SECRET_ACCESS_KEY
+"@ >> ~/.aws/credentials
