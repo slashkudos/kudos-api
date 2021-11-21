8f09f587a2bb6248c34e3db5e397e1dd839540b8
Philip Gai <PhilipMGai@gmail.com>
2021-11-21 05:22:47 +0000

    amplify add api

--- scripts/Update-DevContainer.ps1
+++ scripts/Update-DevContainer.ps1
@@ -1,12 +1,22 @@
-#!/bin/pwsh
-
+#!/usr/bin/env pwsh
 # Post devcontainer create script to run
 
 git config --global pull.rebase true
 sudo npm install -g @aws-amplify/cli
+
+# Setup AWS default profile
+mkdir -p ~/.aws
+
+echo @"
+[default]
+region=us-east-1
+"@ > ~/.aws/config
 
 echo @"
 [default]
 aws_access_key_id = $env:AWS_ACCESS_KEY_ID
 aws_secret_access_key = $env:AWS_SECRET_ACCESS_KEY
 "@ > ~/.aws/credentials
+
+Write-Host "amplify version: " -NoNewLine
+amplify --version
