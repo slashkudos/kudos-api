aba0434c0b4ba51d4d419c338c764ff501deb5fa
Philip Gai <PhilipMGai@gmail.com>
2021-11-23 06:07:04 +0000

    Created Hello World Lambda function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,19 @@ export type AmplifyDependentResourcesAttributes = {
         "kudosapi": {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        },
+        "githubapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "githubWebhookHandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
