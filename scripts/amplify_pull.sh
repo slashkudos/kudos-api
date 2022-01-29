0cbb424d70fbff1137dd0e355b4f1512ba049f12
Philip Gai <philip@slashkudos.com>
2022-01-28 23:02:09 -0600

    Create deploy.yml workflow

---
+++ scripts/amplify_pull.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+amplify_env=$1
+amplify_appId=$2
+
+if [ -z "$amplify_env" ]; then
+    echo "You must provide amplify_env input parameter in order to deploy"
+    exit 1
+fi
+
+if [ -z "$amplify_appId" ]; then
+    echo "You must provide amplify_appId input parameter in order to deploy"
+    exit 1
+fi
+
+if [ -z "$(amplify env get --name $amplify_env | grep 'No environment found')" ]; then
+    echo "Pulling $amplify_env ($amplify_appId)"
+    amplify pull --appId $amplify_appId --envName $amplify_env --yes
+    status=$?
+    exit $status
+else
+    echo "$amplify_env environment does not exist"
+    exit 1
+fi
