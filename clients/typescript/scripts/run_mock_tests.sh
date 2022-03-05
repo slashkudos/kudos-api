aec01c6140de0c2c5d4b39e5e65728b05dcfef60
Philip Gai <PhilipMGai@gmail.com>
2022-03-05 08:23:21 +0000

    Add integration testing to workflows

---
+++ clients/typescript/scripts/run_mock_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Run integration tests against local mock graphql api
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ROOT_DIR=$SCRIPT_DIR/..
+
+export API_URL='http://localhost:20002/graphql'
+export API_KEY='da2-fakeApiId123456'
+
+echo "Starting mock api..."
+while IFS= read -r line; do
+  echo "$line"
+  if [[ "$line" == *"AppSync Mock endpoint is running at "* ]]; then
+    cd $ROOT_DIR
+
+    echo "Executing integration tests..."
+    npm run test:int
+
+    cd - 1>/dev/null
+
+    echo "Terminating mock api..."
+    lsof -ti tcp:20002 | xargs kill
+  fi
+done < <(amplify mock api)
