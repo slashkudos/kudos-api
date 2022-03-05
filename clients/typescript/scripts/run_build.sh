b2e09769b8b4dcb864139063c8f94721eaf3ed75
Philip Gai <PhilipMGai@gmail.com>
2022-03-05 08:53:14 +0000

    Add set -e to all bash scripts

--- clients/typescript/scripts/run_build.sh
+++ clients/typescript/scripts/run_build.sh
@@ -4,6 +4,9 @@
 # so we have to rename tsconfig-build.json to tsconfig.json
 # https://github.com/vercel/ncc/issues/457
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ROOT_DIR=$SCRIPT_DIR/..
 
